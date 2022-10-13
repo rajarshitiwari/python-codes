5644a685e4598d11cab732628543a019a6854609
Rajarshi <rajarshi84@gmail.com>
2022-10-13 12:26:37 +0100

    printing 2d lattice as text

---
+++ lattice/command.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+a2ps -R lattice.txt -o lattice.ps -B --font-size=6 --columns=1
+
---
+++ lattice/lattice.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+n=24
+y=n*"        "+" "
+x=n*"o---"+"o"
+y=n*"|   "+"|"
+print n*(x+"\n"+y+"\n")+x
